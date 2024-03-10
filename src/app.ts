d8308544463c74a012a0af1656883cb295e38775
Yubin <yubinjodev@gmail.com>
2024-03-10 23:10:13 +0900

    Build: Add request logging library

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv'
 import express, { Router } from 'express'
 import proceduresRouter from './routes/procedures'
+import morgan from 'morgan'
 
 dotenv.config()
 
 const app = express()
 const router = Router()
+
+app.use(morgan('dev'))
 
 app.use('/api/v1', router)
 
