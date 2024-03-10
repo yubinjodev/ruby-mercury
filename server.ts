b265a56121827e0839aa9fd1d9cca049f30cf5c5
Yubin <yubinjodev@gmail.com>
2024-03-10 22:10:49 +0900

    Refactor: Simplify server file

--- server.ts
+++ server.ts
@@ -3,14 +3,8 @@ import app from './src/app'
 
 dotenv.config()
 
-app
-  .listen(Number(process.env.PORT), 'localhost', () => {
-    console.info(`ruby-mercury server is live and running on http://localhost:${process.env.PORT}`)
-  })
-  .on('error', (err: any) => {
-    if (err.code === 'EADDRINUSE') {
-      console.error('Address already in use.')
-    } else {
-      console.error(err)
-    }
-  })
+const PORT = Number(process.env.PORT) || 3000
+
+app.listen(PORT, 'localhost', () => {
+  console.info(`ruby-mercury server is live and running on http://localhost:${PORT}`)
+})
